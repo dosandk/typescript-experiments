85dcab5a6bebad00f589239aba0ac1533eded9c2
Volodymyr Shevchuk <Volodymyr.Shevchuk@eleks.com>
2018-07-19 11:56:41 +0300

    Init commit;

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.ts',
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/
+      }
+    ]
+  },
+  resolve: {
+    extensions: [ '.tsx', '.ts', '.js' ]
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, './dist')
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve('./index.html')
+    }),
+  ],
+  devServer: {
+    contentBase: path.join(__dirname, './'),
+    compress: true,
+    port: 9000
+  }
+};
